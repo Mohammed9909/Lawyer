3674e79ee36a9198f2af5e95cfd364d61aea5824
Mohammed <Mohammed.ofisan@hotmail.com>
2019-12-12 15:53:21 +0300

    chang the schema's name

--- app/models/typeOfConsultation.js
+++ app/models/typeOfConsultation.js
@@ -3,18 +3,25 @@ const mongoose = require('mongoose');
 const user = require("./user")
 
 // Define type Of Consultation Schema
-const tocSchema = new mongoose.Schema({
+const typeOfConsultationSchema = new mongoose.Schema({
     user_id: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
     }],
-    name: { type: String, required: true },
-    description: { type: String, default: "" },
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        default: "",
+        require: true
+    },
 });
 
 
 // Compile our Model based on the Schema
-const toc = mongoose.model('Toc', tocSchema);
+const typeOfConsultation = mongoose.model('typeOfConsultation', typeOfConsultationSchema);
 
 // Export our Model for use
-module.exports = toc;
+module.exports = typeOfConsultation;
